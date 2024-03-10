c1e741b421e4d6c6152caa48e799fc2f5ff67780
shi-you-ta <shota_hakamada1993@outlook.com>
2024-03-10 10:44:17 +0900

    未保存ファイルを追加

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        TextBox3 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(29, 12)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Yu Gothic UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(128))
+        Label1.Location = New Point(160, 1)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(36, 38)
+        Label1.TabIndex = 1
+        Label1.Text = "+"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(202, 12)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Yu Gothic UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(128))
+        Label2.Location = New Point(333, 1)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(36, 38)
+        Label2.TabIndex = 3
+        Label2.Text = "="
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(375, 12)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 4
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 55)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(505, 53)
+        Button1.TabIndex = 5
+        Button1.Text = "計算ボタン"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(544, 125)
+        Controls.Add(Button1)
+        Controls.Add(TextBox3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "簡単計算プログラム"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Net
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MessageBox.Show("計算してみよう")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim input1 As Integer
+        Dim input2 As Integer
+        Dim equal As Integer
+
+        input1 = InputCheckFunc(TextBox1.Text, input1)
+        input2 = InputCheckFunc(TextBox2.Text, input2)
+
+        equal = input1 + input2     '代入
+
+        TextBox3.Text = equal.ToString()
+    End Sub
+
+    Private Function InputCheckFunc(textboxvalue As String, checkedvalue As Integer) As Integer
+        If Integer.TryParse(textboxvalue, checkedvalue) = True Then
+            checkedvalue = Integer.Parse(textboxvalue)
+        Else
+            checkedvalue = 0
+        End If
+
+        Return checkedvalue             '呼び出した側に値を返す
+    End Function
+End Class
